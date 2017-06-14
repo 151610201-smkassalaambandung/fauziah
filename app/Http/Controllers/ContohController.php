1de82843eb0d962084f334c2428bebdd0227de90
151610201-smkassalaambandung <151610201@smkassalaambandung.sch.id>
2017-06-14 16:20:54 +0700

    project hari rabu

--- app/Http/Controllers/ContohController.php
+++ app/Http/Controllers/ContohController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class ContohController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function coba()
     {
     	return view('index');
